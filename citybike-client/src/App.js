4fb077030704dbcb06af022987380878abfc200c
Stivenson <stivenson02@gmail.com>
2021-07-18 13:32:55 -0500

    test

--- citybike-client/src/App.js
+++ citybike-client/src/App.js
@@ -1,39 +1,73 @@
-import React, { Component } from "react";
+import React, { Component,useState, useEffect } from "react";
 import socketIOClient from "socket.io-client";
 import { Map, TileLayer, Marker, Popup } from "react-leaflet";
 
 class App extends Component {
+
   constructor() {
     super();
 
     this.state = {
       response: false,
-      endpoint: "http://127.0.0.1:4001",
-      lat: 51.505,
-      lng: -0.09,
-      zoom: 13
+      endpoint: "http://127.0.0.1:4001/",
+      lat: 25.80,
+      lng: -80.20,
+      zoom: 12,
+      stations :[]
     };
 
+
   }
+
   componentDidMount() {
+    const self= this
     const { endpoint } = this.state;
     const socket = socketIOClient(endpoint);
-   
+    socket.on("citybike", function(data){
+      const result = JSON.parse(data)
+      self.setState({stations: result.network.stations})
+    })
   }
+
+
   render() {
     const { response } = this.state;
     const position = [this.state.lat, this.state.lng]
+
+
     return (
 
-      <div className="map">
-        <h1> City Bikes in Miami </h1>
-        <Map center={position} zoom={this.state.zoom}>
-          <TileLayer
-            attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-        </Map>
-      </div>
+        <div className="map">
+          <h1> City Bikes in Miami </h1>
+          <Map center={position} zoom={this.state.zoom}>
+            <TileLayer
+                attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            {
+              response.length > 0 && response.map((station)=>(
+                  <Marker key={station.id} position={[station.latitude, station.longitude]} title={station.name}>
+                    <Popup>
+                        <div>
+                          <div>
+                            {station.name}
+                          </div>
+                          <div>
+                            {station.id}
+                          </div>
+                          <div>
+                            {station.free_bikes}
+                          </div>
+                          <div>
+                            {station.empty_slots}
+                          </div>
+                        </div>
+                    </Popup>
+                  </Marker>
+              ))
+            }
+          </Map>
+        </div>
     );
   }
 }
--- citybike-server/server.js
+++ citybike-server/server.js
@@ -2,13 +2,11 @@ const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
 const citybikeurl = "http://api.citybik.es/v2/networks/decobike-miami-beach"
-
-
+const axios = require("axios");
 
 const port = process.env.PORT || 4001;
 const index = require("./routes/index");
 const app = express();
-
 
 app.use(index);
 
@@ -20,14 +18,44 @@ io.on("connection", socket => {
   var socketId = socket.id;
   var clientIp = socket.request.connection.remoteAddress;
   console.log('New connection ' + socketId + ' from ' + clientIp);
+  socket.emit('initialData', data);
+  socket.on("updateStation", (val) => {
+    data = data.filter(x => x.id !== val.id).concat(val);
+    io.sockets.emit('updateData', data);
+  });
   socket.on("disconnect", () => {
     console.log("Client disconnected");
   });
 });
 
+app.use("/stats", (request, val) => {
+  axios.get(citybikeurl).then(result => {
+    const usageBike = [];
+    result.data.network.stations.map(place => {
+      const allBikes = place.free_bikes + place.empty_slots;
+      const freeBikes = place.free_bikes / allBikes;
+      const emptySlots = place.empty_slots / allBikes;
+      usageBike.push({...place, freeBikes: freeBikes, emptySlots: emptySlots});
+    });
+    const emptyStations = usageBike.sort((a,b) => b.emptySlots - a.emptySlots).slice(0,4);
+    const usedStations = usageBike.sort((a,b) => a.freeBikes - b.freeBikes).slice(0,4);
+    val.setHeader('Content-Type', 'application/json');
+    val.send({usedStations: usedStations, emptyStations: emptyStations}).status(200);
+  }).catch(error => {
+    console.log(error);
+  });
+});
 
+const init = () => {
+  console.log(`Listening on port ${port}`)
+  axios.get(citybikeurl).then(result => {
+    data = result.data.network.stations.map(x => ({lat: x.latitude, long: x.longitude, name: x.name, id: x.id, free_bikes: x.free_bikes, empty_slots: x.empty_slots, is_damaged: false}));
+  }).catch(error => {
+    console.log(error);
+  })
+}
 
-server.listen(port, () => console.log(`Listening on port ${port}`));
+server.listen(port, init );
 
 
 
